7eb8184b4d7e511a00fc2ad2ae042b3f0901e1ff
Hassan Farooqui <hassan.farooqui92@gmail.com>
2018-12-23 21:38:39 +0500

    Adjust grid view for Sale entry page columns

--- IVN_SYS/Sale Entry.Designer.cs
+++ IVN_SYS/Sale Entry.Designer.cs
@@ -34,14 +34,6 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -56,34 +48,26 @@
             this.kgs = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Boxdesc = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.rates = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nos = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Amounts = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tokara = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.rates1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NosTokara = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.amount1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(230, 23);
+            this.textBox1.Location = new System.Drawing.Point(230, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(556, 23);
+            this.textBox2.Location = new System.Drawing.Point(556, 14);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(230, 64);
+            this.textBox3.Location = new System.Drawing.Point(231, 52);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 2;
@@ -91,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(122, 26);
+            this.label1.Location = new System.Drawing.Point(122, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 4;
@@ -100,7 +84,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(122, 68);
+            this.label2.Location = new System.Drawing.Point(116, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 5;
@@ -109,7 +93,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(481, 26);
+            this.label3.Location = new System.Drawing.Point(481, 17);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(30, 13);
             this.label3.TabIndex = 6;
@@ -118,7 +102,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(481, 66);
+            this.label4.Location = new System.Drawing.Point(481, 58);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 7;
@@ -127,7 +111,7 @@
             // CbxPartyName
             // 
             this.CbxPartyName.FormattingEnabled = true;
-            this.CbxPartyName.Location = new System.Drawing.Point(555, 62);
+            this.CbxPartyName.Location = new System.Drawing.Point(555, 53);
             this.CbxPartyName.Name = "CbxPartyName";
             this.CbxPartyName.Size = new System.Drawing.Size(121, 21);
             this.CbxPartyName.TabIndex = 8;
@@ -150,17 +134,9 @@
             this.Itemdesc,
             this.kgs,
             this.rate,
-            this.amount,
-            this.Boxdesc,
-            this.rates,
-            this.nos,
-            this.Amounts,
-            this.tokara,
-            this.rates1,
-            this.NosTokara,
-            this.amount1});
+            this.amount});
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnKeystroke;
-            this.dataGridView1.Location = new System.Drawing.Point(56, 124);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 87);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
@@ -169,7 +145,7 @@
             this.dataGridView1.RowTemplate.DividerHeight = 6;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1137, 224);
+            this.dataGridView1.Size = new System.Drawing.Size(835, 224);
             this.dataGridView1.TabIndex = 35;
             this.dataGridView1.TabStop = false;
             // 
@@ -233,112 +209,11 @@
             this.amount.Name = "amount";
             this.amount.Width = 69;
             // 
-            // Boxdesc
-            // 
-            this.Boxdesc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.Black;
-            this.Boxdesc.DefaultCellStyle = dataGridViewCellStyle7;
-            this.Boxdesc.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
-            this.Boxdesc.DividerWidth = 1;
-            this.Boxdesc.HeaderText = "Box Description";
-            this.Boxdesc.Name = "Boxdesc";
-            this.Boxdesc.Width = 150;
-            // 
-            // rates
-            // 
-            this.rates.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle8.Format = "N6";
-            dataGridViewCellStyle8.NullValue = "0";
-            this.rates.DefaultCellStyle = dataGridViewCellStyle8;
-            this.rates.DividerWidth = 1;
-            this.rates.HeaderText = "Rates";
-            this.rates.Name = "rates";
-            this.rates.Width = 61;
-            // 
-            // nos
-            // 
-            this.nos.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle9.Format = "N6";
-            dataGridViewCellStyle9.NullValue = "0";
-            this.nos.DefaultCellStyle = dataGridViewCellStyle9;
-            this.nos.DividerWidth = 1;
-            this.nos.HeaderText = "Nos";
-            this.nos.Name = "nos";
-            this.nos.Width = 52;
-            // 
-            // Amounts
-            // 
-            this.Amounts.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle10.Format = "N6";
-            dataGridViewCellStyle10.NullValue = "0";
-            this.Amounts.DefaultCellStyle = dataGridViewCellStyle10;
-            this.Amounts.DividerWidth = 1;
-            this.Amounts.HeaderText = "Amount";
-            this.Amounts.Name = "Amounts";
-            this.Amounts.Width = 69;
-            // 
-            // tokara
-            // 
-            this.tokara.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.Black;
-            this.tokara.DefaultCellStyle = dataGridViewCellStyle11;
-            this.tokara.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
-            this.tokara.DividerWidth = 1;
-            this.tokara.HeaderText = "Tokara";
-            this.tokara.Name = "tokara";
-            this.tokara.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.tokara.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.tokara.Width = 150;
-            // 
-            // rates1
-            // 
-            this.rates1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle12.Format = "N6";
-            dataGridViewCellStyle12.NullValue = "0";
-            this.rates1.DefaultCellStyle = dataGridViewCellStyle12;
-            this.rates1.DividerWidth = 1;
-            this.rates1.HeaderText = "Rate";
-            this.rates1.Name = "rates1";
-            this.rates1.Width = 56;
-            // 
-            // NosTokara
-            // 
-            this.NosTokara.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.NosTokara.DefaultCellStyle = dataGridViewCellStyle13;
-            this.NosTokara.DividerWidth = 1;
-            this.NosTokara.HeaderText = "Nos";
-            this.NosTokara.Name = "NosTokara";
-            this.NosTokara.Width = 52;
-            // 
-            // amount1
-            // 
-            this.amount1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            dataGridViewCellStyle14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle14.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle14.Format = "N6";
-            dataGridViewCellStyle14.NullValue = "0";
-            this.amount1.DefaultCellStyle = dataGridViewCellStyle14;
-            this.amount1.DividerWidth = 1;
-            this.amount1.HeaderText = "Amount";
-            this.amount1.Name = "amount1";
-            this.amount1.Width = 69;
-            // 
             // Sale_Entry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1248, 472);
+            this.ClientSize = new System.Drawing.Size(836, 472);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.CbxPartyName);
             this.Controls.Add(this.label4);
@@ -349,6 +224,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Sale_Entry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sale_Entry";
             this.Load += new System.EventHandler(this.Sale_Entry_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -368,18 +244,10 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox CbxPartyName;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Itemcode;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Itemdesc;
-        private System.Windows.Forms.DataGridViewTextBoxColumn kgs;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rate;
         private System.Windows.Forms.DataGridViewTextBoxColumn amount;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Boxdesc;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rates;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nos;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Amounts;
-        private System.Windows.Forms.DataGridViewComboBoxColumn tokara;
-        private System.Windows.Forms.DataGridViewTextBoxColumn rates1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NosTokara;
-        private System.Windows.Forms.DataGridViewTextBoxColumn amount1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kgs;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Itemdesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Itemcode;
     }
 }
